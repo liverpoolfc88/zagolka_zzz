8b398b0f6a252309c6945d7a410363e9a3452a7d
liverpoolfc88 <52122299+liverpoolfc88@users.noreply.github.com>
2020-11-28 17:14:04 +0500

    asd
    sd

--- views/site/card.php
+++ views/site/card.php
@@ -9,29 +9,33 @@ $this->title = Lang::t('Shopping cart');
 
 // echo "<pre>"; var_dump($items->goods[0]->item->template->id); die;
 ?>
-<style >
-#customers {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<style>
+    #customers {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
 
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
+    #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
 
-#customers tr:nth-child(even){background-color: #f2f2f2;}
+    #customers tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
 
-#customers tr:hover {background-color: #ddd;}
+    #customers tr:hover {
+        background-color: #ddd;
+    }
 
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
+    #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
 
 </style>
 <!--<section>-->
@@ -40,10 +44,10 @@ $this->title = Lang::t('Shopping cart');
 <!--        --><?php //foreach ($items->goods as $key=> $item) : ?>
 <!--        <div class="col-sm-6 col-md-4">-->
 <!--            <div class="thumbnail">-->
-<!--                <img src="--><?//= $item->item->photo ?><!--" alt="...">-->
+<!--                <img src="--><? //= $item->item->photo ?><!--" alt="...">-->
 <!--                <div class="caption">-->
-<!--                    <a href="--><?//=Url::to('/?slug='.$item->item->template->slug.'&item_slug='.$item->item->slug)?><!--">-->
-<!--                        <h5>--><?//= $item->item->title?><!--/<span>(--><?//=$item->item->template->title?><!--)</span></h5>-->
+<!--                    <a href="--><? //=Url::to('/?slug='.$item->item->template->slug.'&item_slug='.$item->item->slug)?><!--">-->
+<!--                        <h5>--><? //= $item->item->title?><!--/<span>(--><? //=$item->item->template->title?><!--)</span></h5>-->
 <!--                    </a>-->
 <!--                    <p>...</p>-->
 <!--                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>-->
@@ -66,74 +70,77 @@ $this->title = Lang::t('Shopping cart');
             <th class=""><?= Lang::t('much') ?></th>
             <th class="width-10"><?= Lang::t('Delete') ?></th>
         </tr>
-        <?php if ($items->goods) :?>
-            <?php foreach ($items->goods as $key=> $item) : ?>
+        <?php if ($items->goods) : ?>
+            <?php foreach ($items->goods as $key => $item) : ?>
                 <tr>
                     <td>
-                        <a href="<?=Url::to('/?slug='.$item->item->template->slug.'&item_slug='.$item->item->slug)?>">
+                        <a href="<?= Url::to('/?slug=' . $item->item->template->slug . '&item_slug=' . $item->item->slug) ?>">
                             <img src="<?= $item->item->photo ?>" style="width: 150px">
                         </a>
                     </td>
                     <td>
-                        <a href="<?=Url::to('/?slug='.$item->item->template->slug.'&item_slug='.$item->item->slug)?>">
-                            <h5 class="product_title"><?= $item->item->title?>&nbsp;<span>(<?=$item->item->template->title?>)</span></h5>
+                        <a href="<?= Url::to('/?slug=' . $item->item->template->slug . '&item_slug=' . $item->item->slug) ?>">
+                            <h5 class="product_title"><?= $item->item->title ?>
+                                &nbsp;<span>(<?= $item->item->template->title ?>)</span></h5>
                         </a>
                     </td>
                     <td>
-                        <? if($item->item->sale): ?>
-                            <span class="price-new"><?= number_format($item->item->price * (1 - $item->item->sale/100), 2, ',', ' '); ?>   &nbsp;&nbsp;&nbsp;</span>
+                        <? if ($item->item->sale): ?>
+                            <span class="price-new"><?= number_format($item->item->price * (1 - $item->item->sale / 100), 2, ',', ' '); ?>   &nbsp;&nbsp;&nbsp;</span>
                             <span class="price-old"> <?= number_format($item->item->price, 2, ',', ' '); ?></span>
                         <? else: ?>
                             <span class="price-new"> <?= number_format($item->item->price, 2, ',', ' '); ?></span>
                         <? endif; ?>
                     </td>
-                    <td><?= number_format($item->price * (1 - $item->sale/100) * $item->count, 2, ',', ' '); ?>
+                    <td><?= number_format($item->price * (1 - $item->sale / 100) * $item->count, 2, ',', ' '); ?>
                     </td>
                     <td>
-                        <?php if ($item->pieces==NULL){?>
+                        <?php if ($item->pieces == NULL) { ?>
                             <b>
                                 <?= Lang::t('dona') ?>
                             </b>
-                        <?}else {?>
+                        <? } else { ?>
                             <b>
                                 <?= Lang::t('pachkada') ?>
                             </b>
                             <br>
-                            <?=$item->pieces?>
+                            <?= $item->pieces ?>
                             <?= Lang::t('  ta bor') ?>
-                        <?}?>
-                    </td>
-                    <td >
-                        <input style="width: 100px"    type="number" name="quantity" data-id="<?=$item->item->id?>"
-                                 min="1" class="lolo input-quantity form-control" value="<?=$item->count?>">
+                        <? } ?>
                     </td>
                     <td>
-                        <a class="remove-item remove" href="#"      data-id="<?=$item->good_id?>" data-value="<?= $item->price  ?>" title="<?= Lang::t('Remove Item From Cart') ?>">
-                            <button type="button" data-id="169" title="Remove" class="btn btn-danger delete"><i class="fa fa-times-circle"></i></button>
+                        <input style="width: 100px" type="number" name="quantity" data-id="<?= $item->item->id ?>"
+                               min="1" class="lolo input-quantity form-control" value="<?= $item->count ?>">
+                    </td>
+                    <td>
+                        <a class="remove-item remove" href="#" data-id="<?= $item->good_id ?>"
+                           data-value="<?= $item->price ?>" title="<?= Lang::t('Remove Item From Cart') ?>">
+                            <button type="button" data-id="169" title="Remove" class="btn btn-danger delete"><i
+                                        class="fa fa-times-circle"></i></button>
                         </a>
                     </td>
                 </tr>
             <?php endforeach; ?>
-        <?php endif;?>
+        <?php endif; ?>
         <tfoot>
         <tr style="border-top:solid">
             <td colspan="3"><h4><?= Lang::t('Total Cost') ?></h4></td>
             <td id="cost" colspan="2" class="width-90 summa"><h4><b>0</b></h4></td>
             <td>
-                <!--         <button class="btn btn-danger center update">--><?//=Lang::t('Update')?><!--</button>-->
-                <!--         <input  id="sa" class="btn btn-danger center" type="button" value="--><?//=Lang::t('Update')?><!--">-->
+                <!--         <button class="btn btn-danger center update">--><? //=Lang::t('Update')?><!--</button>-->
+                <!--         <input  id="sa" class="btn btn-danger center" type="button" value="-->
+                <? //=Lang::t('Update')?><!--">-->
             </td>
         </tr>
         </tfoot>
     </table>
 </div>
 
- <div class="">
-    <a class="btn btn-danger left" href="<?//= $this->to('catalog/books') ?>"><?= Lang::t('Continue Shopping') ?></a>
-    <a class="btn btn-danger main-bg right" href="<?=Url::to(['site/complete'])?>"><?= Lang::t('Proceed to Checkout') ?></a>
+<div class="">
+    <a class="btn btn-danger left" href="<? //= $this->to('catalog/books') ?>"><?= Lang::t('Continue Shopping') ?></a>
+    <a class="btn btn-danger main-bg right"
+       href="<?= Url::to(['site/complete']) ?>"><?= Lang::t('Proceed to Checkout') ?></a>
 </div>
-
-
 
 
 <p></p>
@@ -145,16 +152,16 @@ $this->title = Lang::t('Shopping cart');
     $(function () {
         var count = [];
         var id = [];
-        $(".input-quantity").each(function(i){
+        $(".input-quantity").each(function (i) {
             count[i] = $(this).val();
         });
-        $(".input-quantity").each(function(i){
+        $(".input-quantity").each(function (i) {
             id[i] = $(this).data("id");
         });
 
 
-        $.get("/site/update",{item:id, quantity:count},function(response){
-            if(response.result=="success"){
+        $.get("/site/update", {item: id, quantity: count}, function (response) {
+            if (response.result == "success") {
                 $("#cost").html(response.cost);
                 console.log(response.cost);
 
@@ -169,22 +176,22 @@ $this->title = Lang::t('Shopping cart');
         e.preventDefault();
 
         var count = [];
-           var id = [];
-           $(".input-quantity").each(function(i){
-               count[i] = $(this).val();
-           });
-           $(".input-quantity").each(function(i){
-               id[i] = $(this).data("id");
-           });
+        var id = [];
+        $(".input-quantity").each(function (i) {
+            count[i] = $(this).val();
+        });
+        $(".input-quantity").each(function (i) {
+            id[i] = $(this).data("id");
+        });
 
 
-           $.get("/site/update",{item:id, quantity:count},function(response){
-                   if(response.result=="success"){
-                       $("#cost").html(response.cost);
-                       console.log(response.cost);
+        $.get("/site/update", {item: id, quantity: count}, function (response) {
+            if (response.result == "success") {
+                $("#cost").html(response.cost);
+                console.log(response.cost);
 
-                   } else console.log(response.result);
-               });
+            } else console.log(response.result);
+        });
         // console.log(data);
     }
 </script>
